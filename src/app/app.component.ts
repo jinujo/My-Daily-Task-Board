68f6e7daab862b3a170cae66cdca07e3a7390d7f
jinujo <jinuhjoseph@gmail.com>
2020-12-05 17:57:46 -0800

    task modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,24 @@ constructor(private dialog: MatDialog){
   inProgress: Task[] = [];
   done: Task[] = [];
 
-  editTask(list: string, task: Task): void {}
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
+      const dataList = this[list];
+      const taskIndex = dataList.indexOf(task);
+      if (result.delete) {
+        dataList.splice(taskIndex, 1);
+      } else {
+        dataList[taskIndex] = task;
+      }
+    });
+  }
 
   drop(event: CdkDragDrop<Task[]>): void {
     if (event.previousContainer === event.container) {
@@ -54,4 +71,6 @@ constructor(private dialog: MatDialog){
       .afterClosed()
       .subscribe((result: TaskDialogResult) => this.todo.push(result.task));
   }
+
+  
 }
